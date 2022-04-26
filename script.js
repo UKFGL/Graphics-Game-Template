d3ad3dd0e468b9b874e11cff3ab20fe6044f4b6b
UKFGL <96450681+UKFGL@users.noreply.github.com>
2022-04-26 09:30:50 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 // Graphics Library
 
-let cnv = document.getElementById("my-canvas");
-let ctx = cnv.getContext("2d");
-cnv.width = 800;
-cnv.height = 600;
+// Functions List
+// stroke(color)
+// fill(color)
+// lineWidth(width)
+// font(fontSetting)
+// rect(x, y, w, h, mode)
+// line(x1, y1, x2, y2)
+// circle(x, y, r, mode)
+// triangle(x1, y1, x2, y2, x3, y3)
+// text(message, x, y, mode)
+// ellipse(x, y, xRadius, yRadius, rotation, mode)
+// image(img, x, y, w, h)
+// imageClip(img, xc, yc, wc, hc, x, y, w, h)
 
 // Draw a line
-stroke("blue");
-lineWidth(3);
-line(100, 200, 300, 100);
+// stroke("blue");
+// lineWidth(3);
+// line(100, 200, 300, 100);
+
+// Fill Cnavas with Provided Color
+function background(color) {
+  fill(color);
+rect(0, 0, cnv.width, cnv.height, "fill");
+}
 
 // Customize
 function stroke(style) {
@@ -33,9 +48,9 @@ function line(x1, y1, x2, y2) {
 }
 
 // Draw a Rectangle 
-fill("green");
-rect(100, 100, 200, 50, "fill");
-rect(400, 500, 70, 100, "stroke");
+// fill("green");
+// rect(100, 100, 200, 50, "fill");
+// rect(400, 500, 70, 100, "stroke");
 
 // Draw a stroke or fill rectangle with a top-left corner of (x, y) with a width of w and a height of h
 function rect(x, y, w, h, mode) {
@@ -47,8 +62,8 @@ function rect(x, y, w, h, mode) {
 }
 
 // Draw a Circle
-circle(400, 300, 50, "stroke");
-circle(700, 350, 25, "fill");
+// circle(400, 300, 50, "stroke");
+// circle(700, 350, 25, "fill");
 
 
 function circle(x, y, r, mode) {
@@ -62,9 +77,9 @@ function circle(x, y, r, mode) {
 }
 
 // Draw a Triangle
-stroke("purple");
-triangle(0, 400, 300, 400, 250, 450, "fill");
-triangle(0, 300, 400, 300, 350, 250, "stroke");
+// stroke("purple");
+// triangle(0, 400, 300, 400, 250, 450, "fill");
+// triangle(0, 300, 400, 300, 350, 250, "stroke");
 
 function triangle(x1, y1, x2, y2, x3, y3, mode) {
   ctx.beginPath();
@@ -80,10 +95,10 @@ function triangle(x1, y1, x2, y2, x3, y3, mode) {
 }
 
 // Text Function
-font("44px Arial");
-text("Hello World", 500, 200, "fill");
-text("Hello", 500, 300, "stroke");
-fill("red");
+// font("44px Arial");
+// text("Hello World", 500, 200, "fill");
+// text("Hello", 500, 300, "stroke");
+// fill("red");
 
 function font(fontSetting) {
   ctx.font = fontSetting;
@@ -98,12 +113,12 @@ function text(message, x, y, mode) {
 }
 
 // Draw Ellipse
-fill("green");
-ellipse(700, 100, 10, 80, 0, "fill");
+// fill("green");
+// ellipse(700, 100, 10, 80, 0, "fill");
 
-stroke("orange");
-lineWidth(5);
-ellipse(700, 500, 150, 25, 0, "stroke");
+// stroke("orange");
+// lineWidth(5);
+// ellipse(700, 500, 150, 25, 0, "stroke");
 
 function ellipse(x, y, xRadius, yRadius, rotation, mode) {
   ctx.beginPath();
@@ -116,15 +131,15 @@ function ellipse(x, y, xRadius, yRadius, rotation, mode) {
 }
 
 // Draw Image
-let chessImgEl = document.createElement("img");
-chessImgEl.src = "img/chess-img.jpg";
+// let chessImgEl = document.createElement("img");
+// chessImgEl.src = "img/chess-img.jpg";
 
-window.addEventListener("load", draw);
+// window.addEventListener("load", draw);
 
-function draw() {
-  imageClip(chessImgEl, 1000, 600, 300, 200, 500, 300, 300, 200);
-  image(chessImgEl, 0, 0, 300, 200);
-}
+// function draw() {
+//   imageClip(chessImgEl, 1000, 600, 300, 200, 500, 300, 300, 200);
+//   image(chessImgEl, 0, 0, 300, 200);
+// }
 
 function image(img, x, y, w, h) {
   ctx.drawImage(img, x, y, w, h);
